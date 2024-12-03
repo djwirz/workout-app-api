ba2c0128def45e76cefd3c83182392d76564cb39
djwirz <djwirz@gmail.com>
2024-12-03 12:22:39 -0500

    update notion services for new syncing strategy

--- src/services/notion/notionExercises.ts
+++ src/services/notion/notionExercises.ts
@@ -7,13 +7,14 @@ const NOTION_API_URL = "https://api.notion.com/v1/databases";
 const NOTION_DATABASE_ID = process.env.NOTION_EXERCISE_DB_ID;
 const NOTION_API_KEY = process.env.NOTION_API_KEY;
 
-/**
- * Fetches exercises from Notion.
- */
-export async function fetchExercisesFromNotion() {
+export async function fetchExercisesFromNotion(lastSynced: number) {
+  const filter = lastSynced
+    ? { property: "last_edited_time", date: { after: new Date(lastSynced).toISOString() } }
+    : {};
+
   const response = await axios.post(
     `${NOTION_API_URL}/${NOTION_DATABASE_ID}/query`,
-    {},
+    { filter },
     {
       headers: {
         Authorization: `Bearer ${NOTION_API_KEY}`,
@@ -23,12 +24,10 @@ export async function fetchExercisesFromNotion() {
     }
   );
 
-  return response.data.results
-    .map((page: any) => ({
-      id: page.id,
-      name: page.properties.Name.title[0].text.content,
-      group: page.properties.group?.select?.name || "Unknown",
-      video: page.properties.video?.files?.[0]?.file?.url || null,
-    }))
-    .filter(Boolean);
+  return response.data.results.map((page: any) => ({
+    id: page.id,
+    name: page.properties.Name.title[0].text.content,
+    group: page.properties.group?.select?.name || "Unknown",
+    video: page.properties.video?.files?.[0]?.file?.url || null,
+  }));
 }
